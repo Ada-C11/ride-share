4dcf71ce56345f627f6ab285b731beb6d75a1c40
Cloudy Lopez <30698841+CloudyLopez@users.noreply.github.com>
2019-02-19 08:56:57 -0800

    Update a_worksheet.rb

--- a_worksheet.rb
+++ a_worksheet.rb
@@ -1,19 +1,3 @@
-# def num_rides(database, driver_id)
-# end
-# num_rides(drivers_db, "DR0001")
-# {
-#     markers: [
-#     ]
-# }
-# h[:markers]
-
-# def total_num_rides(drivers_db) 
-# #num of rides for each driver
-
-# end
-
-#                  hhhk,k       
-
 drivers_db = {
     DR001: [
       {
@@ -96,6 +80,10 @@ driver2_rides = drivers_db[:DR002]
 driver3_rides = drivers_db[:DR003]
 driver4_rides = drivers_db[:DR004]
 
+# puts "#{driver1_rides.length} rides"
+# puts "#{driver2_rides.length} rides" 
+# puts "#{driver3_rides.length} rides"
+# puts "#{driver4_rides.length} rides"
 allrides = [driver1_rides, driver2_rides, driver3_rides, driver4_rides]
 
 allrides.each_with_index do |rides, i|
@@ -105,18 +93,11 @@ end
 puts ""
 
 # - the total amount of money each driver has made
-# using map (on all rides variable)
-# 
-# allrides.map do |sum, i|
-#     puts  "Driver ##{i+1} #{sum} rides"
-# end
-
 driver1_total = driver1_rides.sum{|ride| ride[:cost].to_i }
 driver2_total = driver2_rides.sum{|ride| ride[:cost].to_i }
 driver3_total = driver3_rides.sum{|ride| ride[:cost].to_i }
 driver4_total = driver4_rides.sum{|ride| ride[:cost].to_i }
 
-# puts blocks, each with index
 puts "The total amount of money Driver 1 made was $#{driver1_total}."
 puts "The total amount of money Driver 2 made was $#{driver2_total}."
 puts "The total amount of money Driver 3 made was $#{driver3_total}."
@@ -125,7 +106,6 @@ puts "The total amount of money Driver 4 made was $#{driver4_total}."
 puts ""
 
 # - the average rating for each driver
-# map
 average1 = driver1_rides.map {|ride| ride[:rating].to_f} 
 average2 = driver2_rides.map {|ride| ride[:rating].to_f} 
 average3 = driver3_rides.map {|ride| ride[:rating].to_f} 
@@ -135,37 +115,31 @@ driver1_avg = (average1.sum / driver1_rides.length).round(2)
 driver2_avg = (average2.sum / driver2_rides.length).round(2)
 driver3_avg = (average3.sum / driver3_rides.length).round(2)
 driver4_avg = (average4.sum / driver4_rides.length).round(2)
-#end
-#total_averages = []
 
 puts ""
 
 puts "Average Ratings"
-
-# EWI
 
 puts "Driver 1: #{driver1_avg}"
 puts "Driver 2: #{driver2_avg}"
 puts "Driver 3: #{driver3_avg}"
 puts "Driver 4: #{driver4_avg}"
 
-total_averages.each_with_index do |rides, i|
-    puts "Driver ##{i+1} #{rides.length} rides"
-end
 
 # - Which driver made the most money?
 
 puts ""
 
-#alltotalsarray
+drivers = ["DR0001", "DR0002", "DR0003", "DR0004"]
+
 drivers_money = [driver1_total, driver2_total, driver3_total, driver4_total]
 
-puts "Driver #{drivers_db[drivers_money.index(drivers_money.max)]} made the most money." 
+puts "Driver #{drivers[drivers_money.index(drivers_money.max)]} made the most money." 
 
-puts $/
+puts ""
 
 # - Which driver has the highest average rating?
 
 drivers_rating = [driver1_avg, driver2_avg, driver3_avg, driver4_avg ]
 
-puts "Driver #{drivers_db[drivers_rating.index(drivers_rating.max)]} has the highest rating average." 
+puts "Driver #{drivers[drivers_rating.index(drivers_rating.max)]} has the highest rating average." 
