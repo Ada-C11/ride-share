8321aa4a7db273e027bb4775e9b0882730720fa1
aphunk <41399466+aphunk@users.noreply.github.com>
2019-02-18 21:02:13 -0800

    Update rideshare.rb

--- rideshare.rb
+++ rideshare.rb
@@ -1,4 +1,4 @@
-@rideshare_info = {
+rideshare_info = {
   DR0001: [
     {date: "3rd Feb 2016", cost: 10, rider: "RD0022", rating: 5},
     {date: "3rd Feb 2016", cost: 30, rider: "RD0015", rating: 4},
@@ -20,32 +20,31 @@
     {date: "3rd Feb 2016", cost: 5, rider: "RD0022", rating: 5},
     {date: "4th Feb 2016", cost: 10, rider: "RD0022", rating: 4},
     {date: "5th Feb 2016", cost: 20, rider: "RD0073", rating: 5},
-
   ],
 }
 
-@rideshare_info.map do |driver, rides|
+rideshare_info.each do |driver, rides|
   ride_count = rides.count
   puts "#{driver} gave #{rides.count} rides"
 end
 
-def added_ride_info(key)
+def added_ride_info(mama_array, key)
   new_hash = {}
-  @rideshare_info.each do |driver, rides|
+  mama_array.each do |driver, rides|
     total = rides.sum { |ride| ride[key] }
     new_hash[driver] = total
   end
   return new_hash
 end
 
-earnings_hash = added_ride_info(:cost)
+earnings_hash = added_ride_info(rideshare_info, :cost)
 earnings_hash.each do |driver, earnings|
   puts "#{driver} made a total of $#{"%.2f" % earnings}"
 end
 
-ratings_hash = added_ride_info(:rating)
+ratings_hash = added_ride_info(rideshare_info, :rating)
 ratings_hash.each do |driver, ratings|
-  ratings_hash[driver] = ratings.to_f / @rideshare_info[driver].length
+  ratings_hash[driver] = (ratings.to_f) / (rideshare_info)[driver].length
   puts "#{driver} has an average rating of #{"%.1f" % ratings_hash[driver]}"
 end
 
@@ -54,4 +53,5 @@ highest_earner = earnings_hash.max_by { |driver, earnings| earnings }
 highest_rated = ratings_hash.max_by { |driver, rating| rating }
 
 puts "================= THE WINNERS ================="
-puts "#{highest_earner[0]} is the highest earner with $#{"%.2f" % highest_earner[1]}, and #{highest_rated[0]} is the highest rated with #{"%.1f" % highest_rated[1]}!"
+puts "#{highest_earner[0]} is the highest earner with $#{"%.2f" % highest_earner[1]}, and #{highest_rated[0]} 
+is the highest rated with #{"%.1f" % highest_rated[1]}!"
